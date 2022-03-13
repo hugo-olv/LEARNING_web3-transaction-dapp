952230f8302a338caded28055dc298c0b7fb8655
themaskedcat <oliveira.developpement@gmail.com>
2022-03-13 19:02:04 +0100

    Client config init

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,7 @@
+
+export const Loader = () => {
+    return (
+        <h1>Loader</h1>
+    )
+}
+
---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,6 @@
+
+export const Welcome = () => {
+    return (
+        <h1>Welcome</h1>
+    )
+}
