9b75ada4e22848dd8dca8c5e3836984bfffa10b4
themaskedcat <oliveira.developpement@gmail.com>
2022-03-13 22:08:33 +0100

    Upgrade tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  mode: "jit",
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     fontFamily: {
       display: ["Open Sans", "sans-serif"],
@@ -27,9 +26,6 @@ module.exports = {
         "slide-in": "slide-in 0.5s ease-out",
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [require("@tailwindcss/forms")],
 };
